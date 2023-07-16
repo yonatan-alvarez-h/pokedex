1cabcf39fa78a545f555812f985dfea3fd37e6ab
Yonatan Alvarez H <yonatan.alvarez.h@gmail.com>
2023-07-15 20:45:26 -0500

    Ajuste en el estilo de la flecha atras en el screen de Pokemon

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { ScrollView } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
 import { getPokemonDetailsApi } from '../api/pokemon';
 import Header from "../components/Pokemon/Header";
 import Type from "../components/Pokemon/Type";
@@ -13,7 +14,22 @@ export default function Pokemon(props) {
     } 
   } = props;
 
-  const [pokemon, setPokemon] = useState(null)
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => null,
+      headerLeft: () => (
+        <Icon
+          name="arrow-left"
+          color="#fff"
+          size={20}
+          style={{ marginLeft: 20 }}
+          onPress={navigation.goBack}
+        />
+      ),
+    });
+  }, [navigation, params]);
 
   useEffect(() => {
     (async () => {
